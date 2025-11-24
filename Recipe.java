ed7a065ef79d6197a63a8598eefc9a78204e5cae
Gianluca Zambito <gzambito28@gmail.com>
2025-11-24 14:20:26 -0500

    Initial sharing of project

---
+++ Recipe.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Recipe here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Recipe
+{
+    /**
+     * Constructor for objects of class Recipe
+     */
+    public Recipe()
+    {
+    }
+
+}
