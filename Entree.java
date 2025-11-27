3b2bbd90696ccf7ddd7b6e129286b8807aa12b8e
Gianluca Zambito <gzambito28@gmail.com>
2025-11-27 14:12:23 -0500

    Added subclasses

---
+++ Entree.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Entree here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Entree extends Recipe
+{
+
+    /**
+     * Constructor for objects of class Entree
+     */
+    public Entree()
+    {
+
+    }
+
+}
