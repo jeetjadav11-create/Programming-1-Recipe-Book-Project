be3574c85983c94df8fe0707c989bd9efe93a809
Jeet Jadav <jeetjadav11@gmail.com>
2025-12-01 13:52:04 -0500

    Started on recipeBok

--- RecipeBook.java
+++ RecipeBook.java
@@ -3,28 +3,33 @@ import java.util.*;
 public class RecipeBook{
     private String author;
     private List<Recipe> recipes;
-    
+
     public RecipeBook(String author){
         this.author = author;
     }
-    
+
     public String getAuthor(){
         return author;
     }
-    
+
     public void printRecipe(){
-        
+
     }
-    
+
     public void printAllRecipes(){
-    
+        if (recipes.isEmpty()) {
+            System.out.println("No recipes in the book.");
+            return;
+        }
     }
-    
-    public void addRecipe(){
-        
+
+    public void addRecipe(Recipe r){
+        if (r != null) {
+            recipes.add(r);  
+        }
     }
-    
-    public void removeRecipe(){
-        
+
+    public void removeRecipe(Recipe r){
+        recipes.remove(r);
     }
 }
